8eb397aa3d603586c9670ccb989fa2a4b640e46c
vuhayhat <vuhayhatmtp@gmail.com>
2025-12-17 13:35:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -126,8 +126,8 @@ async function maybeSeedStudent() {
 const port = process.env.PORT || 3000
 
 Promise.all([maybeSeedAdmin(), maybeSeedStudent()]).finally(() => {
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`)
   })
 })
 
